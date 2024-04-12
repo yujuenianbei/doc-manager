038b90a6dc41a877c36cc4ad705880d9a9a2a809
wad01602111 <wad01602111@alibaba-inc.com>
2024-04-12 18:31:55 +0800

    123123

---
+++ assets/F42D53A6-7E3A-454E-85CF-D0A31BFF4346-tOAPem-i.js
@@ -0,0 +1,1 @@
+const s="/doc-manager/assets/F42D53A6-7E3A-454E-85CF-D0A31BFF4346-BFyQZJ_C.png";export{s as _};
--- registerSW.js
+++
@@ -1,1 +0,0 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/doc-manager/sw.js', { scope: '/doc-manager/' })})}
